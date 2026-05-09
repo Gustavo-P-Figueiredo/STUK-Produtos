d77a12cbb0dce234eb8349f6afc134b65e7bde6b
Gustavo-P-Figueiredo <gustavo.pereira1204@hotmail.com>
2026-05-09 19:00:31 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/GusFigue/example/STUK_Produtos/StukProdutosApplication.java
@@ -0,0 +1,13 @@
+package GusFigue.example.STUK_Produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StukProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StukProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GusFigue/example/STUK_Produtos/StukProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package GusFigue.example.STUK_Produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StukProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
